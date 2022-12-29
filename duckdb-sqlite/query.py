b54a316f5ee59ad6428a969427589917cdd97658
= <alexm91@vt.edu>
2022-12-29 16:12:17 -0700

    print if exception occurs in Python

--- duckdb-sqlite/query.py
+++ duckdb-sqlite/query.py
@@ -3,15 +3,19 @@
 import duckdb
 import timeit
 import sys
+import traceback
 
-query = sys.stdin.read()
-print(query)
+try: 
+    query = sys.stdin.read()
+    print(query)
 
-con = duckdb.connect(database="my-db.duckdb", read_only=False)
-con.execute("LOAD sqlite_scanner;")
-for try_num in range(3):
-    start = timeit.default_timer()
-    results = con.execute(query).fetchall()
-    end = timeit.default_timer()
-    print(end - start)
-    del results
+    con = duckdb.connect(database="my-db.duckdb", read_only=False)
+    con.execute("LOAD sqlite_scanner;")
+    for try_num in range(3):
+        start = timeit.default_timer()
+        results = con.execute(query).fetchall()
+        end = timeit.default_timer()
+        print(end - start)
+        del results
+except:
+    traceback.print_exc()
