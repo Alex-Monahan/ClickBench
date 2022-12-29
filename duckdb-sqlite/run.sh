3ebaf5b51bb18a30bddc0cebe8f9b0f9c15966f0
= <alexm91@vt.edu>
2022-12-29 15:22:31 -0700

    revert run.sh to duckdb copy

--- duckdb-sqlite/run.sh
+++ duckdb-sqlite/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sudo cat queries.sql | while read query; do
+cat queries.sql | while read query; do
     sync
     echo 3 | sudo tee /proc/sys/vm/drop_caches >/dev/null
 
     # We need to restart and reinitialize DuckDB after every query,
     # because it often fails with Segmentation fault (core dumped)
-    sudo python3 ./query.py <<< "${query}"
+    ./query.py <<< "${query}"
 done
